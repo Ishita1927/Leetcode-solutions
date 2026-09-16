2b4eb9e4cd0132af38d1c673f8e29d9dd1604c25
Ishita1927 <ishik.1927@gmail.com>
2026-09-16 16:47:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.46%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int n = x;
+        int revNum = 0;
+        
+        while (n != 0) {
+            int ld = n % 10;
+            
+            if (revNum > INT_MAX / 10 || revNum < INT_MIN / 10) {
+                return 0;
+            }
+            
+            revNum = (revNum * 10) + ld;
+            n = n / 10;
+        }
+        
+        return revNum;
+    }
+};
